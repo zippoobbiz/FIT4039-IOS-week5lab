cae5d7bd05bf807d27c8e72ee3d6d055a12c2bd0
Xiaoduo <zippoobbiz@gmail.com>
2015-04-08 21:49:11 +1000

    Initial Commit

---
+++ week5lab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  week5lab
+//
+//  Created by BigBadEgg on 4/8/15.
+//  Copyright (c) 2015 Xiaoduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ week5lab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  week5lab
+//
+//  Created by BigBadEgg on 4/8/15.
+//  Copyright (c) 2015 Xiaoduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
